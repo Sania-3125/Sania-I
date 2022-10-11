1615e19860397f0faa7038249ce2f7190c5fc133
Sania-3125 <115518407+Sania-3125@users.noreply.github.com>
2022-10-11 08:54:03 +0530

    Add files via upload

---
+++ dm prac 6 sania.R
@@ -0,0 +1,39 @@
+COS WAVE
+x=seq(0,2*pi,0.01)
+y=cos(x)
+plot(x,y,type="l")
+
+
+TAN WAVE
+x=seq(0,2*pi,0.01)
+y=tan(x)
+plot(x,y,type="l")
+
+
+ABSOLUTE WAVE
+x=seq(-5,5,0.05)
+y=abs(x)
+plot(x,y,type="l")
+
+CEILING WAVE
+x=seq(-5,5,0.05)
+y=ceiling(x)
+plot(x,y,type="l")
+
+
+FLOOR WAVE
+x=seq(-5,5,0.05)
+y=floor(x)
+plot(x,y,type="l")
+
+
+LOGARITHMIC WAVE
+x=seq(10,100,5)
+y=log(x)
+plot(x,y,type="l")
+
+
+EXPONENTIAL WAVE
+x=seq(10,100,5)
+y=exp(x)
+plot(x,y,type="l")
